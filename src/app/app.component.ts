3273c9e5015ce30689ce46ebfe20e6918513851f
Paulo Tenório <paulorltenorio@gmail.com>
2025-07-15 01:49:43 -0300

    Implementa layout da Home com Bootstrap e cards de navegação para temas bíblicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,10 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  // O template abaixo contém apenas o <router-outlet></router-outlet>, que é o ponto onde o Angular
+  // irá renderizar dinamicamente o componente correspondente à rota atual definida em app.routes.ts
+  template: '<router-outlet></router-outlet>',
+  styleUrls: ['./app.component.scss']  
 })
 export class AppComponent {
   title = 'reflexoes-biblicas';
