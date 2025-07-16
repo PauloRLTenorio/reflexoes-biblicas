4e35776a676a44ca7e292a7337d03077a78ba1ae
Paulo Tenório <paulorltenorio@gmail.com>
2025-07-16 01:19:24 -0300

    feat(header): cria componente Header standalone com navegação para páginas de temas e atualiza configurações globais do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
-  // O template abaixo contém apenas o <router-outlet></router-outlet>, que é o ponto onde o Angular
-  // irá renderizar dinamicamente o componente correspondente à rota atual definida em app.routes.ts
-  template: '<router-outlet></router-outlet>',
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+  <app-header></app-header>
+  <router-outlet></router-outlet>
+`,
   styleUrls: ['./app.component.scss']  
 })
 export class AppComponent {
